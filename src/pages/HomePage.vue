1bc394439cdc807b007187aa128ad4af03228d15
Gabrieldrf <gabrieldarosafarias@gmail.com>
2026-06-07 04:15:06 -0300

    Commit Inicial do Projeto

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <h1>Olá Quasar!</h1>
+      <q-btn color="primary" label="Clique Aqui" />
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+</script>
