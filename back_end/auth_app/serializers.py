b72a5e60e385497ba8ca4ed8f2898568b15bb3cb
Gabriel Farias <gabrieldarosafarias@gmail.com>
2026-06-09 00:01:38 -0300

    feat: integração inicial Django e Quasar

--- back_end/auth_app/serializers.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
